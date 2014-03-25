8a24c796e997ec50b8c057e4b9c3d97ae2bf8f4c
Mohd Khairul Ikhwan Kamarudin <petalyaa@gmail.com>
2014-03-25 13:08:45 +0800

    Remove Unnecessary Print
    Remove Unnecessary Print

--- src/org/pet/raspservice/web/controller/IRController.java
+++ src/org/pet/raspservice/web/controller/IRController.java
@@ -9,12 +9,12 @@ public class IRController implements RaspService {
 
 	@Override
 	public void handleGet(HttpServletRequest request, HttpServletResponse response) {
-		System.err.println("Get inside ir controller");
+		
 	}
 
 	@Override
 	public void handlePost(HttpServletRequest request, HttpServletResponse response) {
-		System.err.println("Post inside ir controller");
+		
 	}
 
 }
